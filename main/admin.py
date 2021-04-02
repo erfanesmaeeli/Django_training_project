2525b0cf2239fe5898df57cb223b9969a1b15793
erfan esmaeeli <bestwork.erfan@gmail.com>
2021-04-02 22:06:42 +0430

    fix bugs

--- main/admin.py
+++ main/admin.py
@@ -3,14 +3,13 @@ from main.models import Blog
 
 
 class BlogAdmin(admin.ModelAdmin):
-	list_display = ['title','author', 'price', 'state']
-	list_filter = ('state', )
-	search_fields = ('title', 'price')
-	readonly_fields = ('title', )
-	list_editable = ('price', 'state')
-	ordering = ('title', '-price')
-	sortable_by = ('title', )
-
+    list_display = ['title', 'author', 'price', 'state']
+    list_filter = ('state', )
+    search_fields = ('title', 'price')
+    readonly_fields = ('title', )
+    list_editable = ('price', 'state')
+    ordering = ('title', '-price')
+    sortable_by = ('title', )
 
 
 admin.site.register(Blog, BlogAdmin)
--- main/models.py
+++ main/models.py
@@ -8,13 +8,13 @@ STATE_TYPES = (
     ('R', 'عدم تایید')
 )
 
+
 class Blog(models.Model):
     title = models.CharField(max_length=120, null=True, unique=False, verbose_name="عنوان")
     author = models.ForeignKey(User, related_name='blogs', on_delete=models.CASCADE, null=True, verbose_name="نویسنده")
     description = models.TextField(verbose_name="توضیحات")
     price = models.IntegerField(default=0, null=True, verbose_name="قیمت")
     state = models.CharField(max_length=1, choices=STATE_TYPES, default='P', null=True, verbose_name="وضعیت")
-
 
     def __str__(self):
         return str(self.id)
